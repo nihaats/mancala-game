c87dd899ffe3832061c1b5f6807d3b6dc0343243
Nihat S <nhtsarimehmet@gmail.com>
2022-09-24 12:31:42 +0300

    first-player.component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
+import { FirstPlayerComponent } from './pages/first-player/first-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HowToPlayComponent
+    HowToPlayComponent,
+    FirstPlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/first-player/first-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-player',
+  templateUrl: './first-player.component.html',
+  styleUrls: ['./first-player.component.css']
+})
+export class FirstPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
