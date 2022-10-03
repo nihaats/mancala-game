9647ac96609e32fd20e80e560511f4de5c36dfa6
Nihat S <nhtsarimehmet@gmail.com>
2022-10-03 16:38:48 +0300

    component is updated

--- src/app/pages/first-player/first-player.component.ts
+++ src/app/pages/first-player/first-player.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-first-player',
@@ -8,180 +7,232 @@ import { MainService } from 'src/app/services/main.service';
 })
 export class FirstPlayerComponent implements OnInit {
 
-  store1IndexNumber: number = 6;
-  store2IndexNumber: number = 13;
+  store1Index: number = 0;
+  store2Index: number = 1;
   defaultStoneCount: number = 4;
   defaultHoleCount: number = 12;
-  holesCount: any[] = [];
-
-  store1: string[] = [];
-  store2: string[] = [];
-
-
-  isPlayer1: boolean = false;
+  initalValue: number = 5;
+  holes: any[] = [];
+  stores: any[] = [];
+  isPlayer1: boolean = true;
   isPlayer2: boolean = false;
+  oppositeIndex: number = 0;
 
-  constructor(
-    public mainService: MainService
-  ) { }
-
+  //* begin:: Inital Settings
   ngOnInit(): void {
     this.createHoles();
+    this.createStores();
   }
 
   createHoles(){
     for (let i = 0; i < this.defaultHoleCount; i++) {
-      this.holesCount.push([]);
+      this.holes.push([]);
     }
     this.distributeStones();
   }
 
-  setColors(randomNumber: number){
-    return this.mainService.colors[randomNumber];
+  createStores(){
+    for (let i = 0; i < 2; i++) {
+      this.stores.push([]);
+    }
   }
 
   distributeStones(){
     for (let i = 0; i < this.defaultStoneCount; i++) {
-      this.holesCount.forEach(x => {
-        const randNumber = Math.floor(Math.random() * this.mainService.colors.length);
+      this.holes.forEach(x => {
+        const randNumber = Math.floor(Math.random() * this.colors.length);
         x.push(this.setColors(randNumber))
       });
     }
   }
 
-  // todo: 2 tane parametre geçilecek player1 = false, player2 = false
-  // turn(player1: boolean = false, player2: boolean = false){
-  //   if (playerNumber == 1) {
-  //     this.isPlayer1 = true;
-  //     this.isPlayer2 = false;
+  colors: string[] = [
+    '#E53535', //Kırmızı
+    '#D71AD0', //Pembe
+    '#1985F0', //Mavi
+    '#19F04C', //Yeşil
+    '#F8FF2F',  //Sarı
+    '#52012a', //Bordo
+    '#7cb9ef', //Mavi
+    // '#000'
+  ]
+
+  setColors(randomNumber: number){
+    return this.colors[randomNumber];
+  }
+
+  turn(isPlayer1: boolean = false, isPlayer2: boolean = false){
+    this.isPlayer1 = isPlayer1;
+    this.isPlayer2 = isPlayer2;
+    console.log("player1: " + this.isPlayer1);
+    console.log("player2: " + this.isPlayer2);
+  }
+  //* end:: Inital Settings
+
+  playerClicked(mainIndex: number, currentIndex: number, count: number){
+    this.oppositeIndex = mainIndex < 6 ? (10 - currentIndex) : (currentIndex - 5)
+    switch (currentIndex + count) {
+      //* 1) #FirstAction taş sayısı 1 ise ve bir sonraki yuva, store1 ise
+      case 6:
+        this.stoneCountIsOneAndNextIndexIsStore(mainIndex, currentIndex, count);
+        break;
+
+      //* 2) #SecondAction taş sayısı 1'den fazla ise ve son yuva, store1 ise
+      case 7:
+        this.stoneCountIsMoreThanOneAndNextIndexIsStore(mainIndex, currentIndex, count);
+        break;
+
+      //* 3) #ThirdAction taş sayısı 1 ise ve bir sonraki yuva, store2 ise
+      case 12:
+        this.stoneCountIsOneAndNextIndexIsStore(mainIndex, currentIndex, count);
+        break;
+
+      //* 4) #FourthAction taş sayısı 1'den fazla ise ve son yuva, store2 ise
+      case 13:
+        this.stoneCountIsMoreThanOneAndNextIndexIsStore(mainIndex, currentIndex, count);
+        break;
+
+      //* 5) #DefaultAction taş sayısı 1'den fazla ise ve son yuva, store2 ise
+      default:
+        this.generalClicked(mainIndex, currentIndex, count)
+        break;
+    }
+  }
+
+  stoneCountIsOneAndNextIndexIsStore(mainIndex: number, currentIndex: number, count: number){
+    if(count == 1)
+      this.pushOneToStore(mainIndex);
+    else
+      this.generalClicked(mainIndex, currentIndex, count);
+  }
+
+  stoneCountIsMoreThanOneAndNextIndexIsStore(mainIndex: number, currentIndex: number, count: number){
+    if(count > 1 && count < 8)
+      this.pushMoreThanOneToStore(mainIndex);
+    else
+      this.generalClicked(mainIndex, currentIndex, count);
+  }
+
+  generalClicked(mainIndex: number, currentIndex: number, count: number){
+    if(count == 1) {
+      const nextHoleIndex: number = mainIndex < 6 ? (mainIndex - 1) : (currentIndex + 1);
+
+      if(this.holes[nextHoleIndex].length == 0 && this.holes[this.oppositeIndex].length == 0){
+        this.nextIndexEqual0AndOppositeIndexEqual0(nextHoleIndex, mainIndex);
+        return;
+      }
+      if(this.holes[nextHoleIndex].length == 0 && this.holes[this.oppositeIndex].length != 0){
+        this.nextIndexEqual0AndOppositeIndexNotEqual0(nextHoleIndex, mainIndex)
+        return;
+      }
+      if(this.holes[nextHoleIndex].length != 0){
+        this.nextIndexNotEqual0(nextHoleIndex, mainIndex)
+        return;
+      }
+    }
+    if(count > 1){
+      // this.stoneCountsMoreThanOne(mainIndex, currentIndex, count);
+      if((currentIndex + count >= 0 && currentIndex + count <= 6) || (currentIndex + count > 7 && currentIndex + count <= 13)){
+        this.stoneCountIsMoreThanOneAndNextIndexIsNotStore(mainIndex, currentIndex, count)
+      }
+      if(currentIndex + count > 13){
+        this.stoneCountIsMoreThanOneAndNextIndexIsNotStore(mainIndex, currentIndex, count);
+      }
+
+    }
+  }
+
+  pushOneToStore(i: any){
+    const storeIndex = i < 6 ? this.store1Index : this.store2Index
+    this.stores[storeIndex].push(this.holes[i].shift());
+    storeIndex == this.store1Index ? this.turn(true) : this.turn(false, true)
+  }
+
+  pushMoreThanOneToStore(mainIndex: number){
+    const lastStone: string = this.holes[mainIndex].splice(-1);
+    const newHoles = JSON.parse(JSON.stringify(this.holes));
+    this.holes[mainIndex] = []
+    for (let i = 0; i < newHoles[mainIndex].length; i++) {
+      this.holes[mainIndex < 6 ? mainIndex - i : mainIndex + i].push(newHoles[mainIndex][i]);
+    }
+    this.stores[mainIndex < 6 ? this.store1Index : this.store2Index].push(lastStone);
+    mainIndex < 6 ? this.turn(true) : this.turn(false, true);
+  }
+
+  //* begin:: #DefaultAction - taş sayısı 1 ise
+  nextIndexEqual0AndOppositeIndexEqual0(nextHoleIndex: number, mainIndex: number){
+    this.holes[nextHoleIndex].push(this.holes[mainIndex].shift());
+    mainIndex < 6 ? this.turn(false, true) : this.turn(true);
+  }
+
+  nextIndexEqual0AndOppositeIndexNotEqual0(nextHoleIndex: number, mainIndex: number){
+    const storeIndex: number = mainIndex < 6 ? this.store1Index : this.store2Index;
+    this.stores[storeIndex].push(this.holes[mainIndex].shift());
+    this.holes[this.oppositeIndex].forEach((x: string) => {
+      this.stores[storeIndex].push(x);
+    });
+    this.holes[this.oppositeIndex] = [];
+    mainIndex < 6 ? this.turn(false, true) : this.turn(true);
+  }
+
+  nextIndexNotEqual0(nextHoleIndex: number, mainIndex: number){
+    this.holes[nextHoleIndex].push(this.holes[mainIndex].shift());
+    mainIndex < 6 ? this.turn(false, true) : this.turn(true);
+  }
+  //* end:: #DefaultAction - taş sayısı 1 ise
+
+  // begin:: #DefaultAction - taş sayısı 1'den fazla ise
+  // stoneCountsMoreThanOne(mainIndex: number, currentIndex: number, count: number){
+  //   if((currentIndex + count >= 0 && currentIndex + count <= 6) || (currentIndex + count > 7 && currentIndex + count <= 13)){
+  //     this.stoneCountIsMoreThanOneAndNextIndexIsNotStore(mainIndex, currentIndex, count)
   //   }
-  //   else{
-  //     this.isPlayer1 = false;
-  //     this.isPlayer2 = true;
+  //   if(currentIndex + count > 13){
+  //     this.stoneCountIsMoreThanOneAndNextIndexIsNotStore(mainIndex, currentIndex, count);
   //   }
   // }
 
-  player1Clicked(i: any,  count: number){
-    console.log("index: " + i);
-    console.log("count: " + count);
-    // const player1Holes = this.mainService.player1Holes;
-    // const player2Holes = this.mainService.player2Holes;
+  stoneCountIsMoreThanOneAndNextIndexIsNotStore(mainIndex: number, currentIndex: number, count: number){
+    const newHoles = JSON.parse(JSON.stringify(this.holes));
+    this.holes[mainIndex] = [];
+    let k = 0;
 
-    // //When 1 stone in the hole moves, it gives the hole index opposite the next hole.
-    // const oppositeIndex = 4 - i;
-    // //if there is only 1 stone in the hole
-    // if (count == 1) {
-    //   if ((i+1) != this.store1IndexNumber) {
-    //     //if the count of stones in the hole is not 0 and next hole is not store
-    //     if(player1Holes[i+1].length != 0){
-    //       let removedItem = player1Holes[i].shift();
-    //       player1Holes[i+1].push(removedItem);
-    //       this.mainService.player1 = false;
-    //       this.mainService.player2 = true;
-    //     }
+    // current = 5
+    // count = 9
 
-    //     //if the count of stones in the hole is equal 0 and next hole is not store
-    //     if(player1Holes[i+1].length == 0){
-    //       //if the count of stones in the opposite hole is not 0
-    //       if(player2Holes[oppositeIndex].length != 0){
-    //         //We also throw the stones of the opposite hole into our own store.
-    //         //when we move the last stone from the current hole to our next empty hole
-    //         player2Holes[oppositeIndex].forEach((x: string) => {
-    //           this.mainService.store1.push(x);
-    //         });
-    //         player2Holes[oppositeIndex]  = []
-    //         this.mainService.store1.push(player1Holes[i].shift());
+    for (let i = 0; i < newHoles[mainIndex].length; i++) {
+      if(currentIndex < 6){
 
-    //         this.mainService.finishGame();
-    //         this.mainService.player1 = false;
-    //         this.mainService.player2 = true;
-    //         return;
-    //       }
-    //       //If there is no stone in the opposite hole, our own stone stays where it is and it is the opponent's turn.
-    //       // when we move the last stone from the current hole to our next empty hole
-    //       if(player2Holes[oppositeIndex].length == 0){
-    //         player1Holes[i+1].push(player1Holes[i].shift());
-    //         this.mainService.player1 = false;
-    //         this.mainService.player2 = true;
-    //         return;
-    //       }
-    //     }
-    //   }
+        if(currentIndex + 1 + i < 7){
+          this.holes[mainIndex - i].push(newHoles[mainIndex][i]);
+        }
 
-    //   //If the next nest is our store, we leave the last stone in our store and it's our turn again.
-    //   if((i+1) == this.store1IndexNumber){
-    //     let removedItem = player1Holes[i].shift();
-    //     this.mainService.store1.push(removedItem);
-    //     this.mainService.player1 = true;
-    //     this.mainService.player2 = false;
-    //     this.mainService.finishGame();
-    //   }
-    // }
-    // //if there are more than 1 stones in the hole
-    // else {
-    //   let itemsGroup: any = []
-    //   player1Holes[i].forEach((x: string) => {
-    //     itemsGroup.push(x);
-    //   })
-    //   player1Holes[i] = [];
+        if(currentIndex + 1 + i == 7){
+          this.stores[this.store1Index].push(newHoles[mainIndex][i]);
+        }
 
-    //   let end = (i+count)-1;
-    //   let index2 = 0;
-    //   let indexTest = 0
-    //   let k = 0;
-    //   let currentIndex = 0;
-    //   for (let index = i ; index <= end ; index++) {
-    //     if (index < this.store1IndexNumber){
-    //       player1Holes[index].push(itemsGroup[index2]);
-    //       if(index == end){
-    //         this.mainService.player1 = false;
-    //         this.mainService.player2 = true;
-    //       }
-    //     }
+        if(currentIndex + 1 + i > 7){
+          this.holes[(currentIndex - 1) + i].push(newHoles[mainIndex][i]);
+        }
 
-    //     if (index == this.store1IndexNumber){
-    //       this.mainService.store1.push(itemsGroup[index2]);
-    //       if(index == end){
-    //         this.mainService.player1 = true;
-    //         this.mainService.player2 = false;
-    //       }
-    //     }
+      }
+      else{
 
-    //     if (index > this.store1IndexNumber && index < this.store2IndexNumber) {
-    //       this.mainService.player2Holes[indexTest].push(itemsGroup[index2]);
+        if (currentIndex + i < 12) {
+          this.holes[currentIndex + i].push(newHoles[mainIndex][i]);
+        }
 
-    //       if(index == end){
-    //         let endLength = this.mainService.player2Holes[indexTest].length;
-    //         currentIndex = end - 7;
-    //         //If the number of stones in the slot where the last stone fell is even
-    //         if( endLength % 2 == 0){
-    //           this.mainService.player2Holes[currentIndex].forEach((x: string) => {
-    //             this.mainService.store1.push(x);
-    //           });
-    //           this.mainService.player2Holes[currentIndex] = [];
-    //           this.mainService.player1 = false;
-    //           this.mainService.player2 = true;
-    //         }
-    //         if( endLength % 2 == 1){
-    //           this.mainService.player1 = false;
-    //           this.mainService.player2 = true;
-    //         }
-    //       }
-    //       indexTest += 1;
-    //     }
+        if (currentIndex + i == 12) {
+          this.stores[this.store2Index].push(newHoles[mainIndex][i]);
+        }
 
-    //     if (index >= this.store2IndexNumber) {
-    //       player1Holes[k].push(itemsGroup[index2]);
-    //       k += 1;
-    //       if(index == end){
-    //         this.mainService.player1 = false;
-    //         this.mainService.player2 = true;
-    //       }
-    //     }
+        if (currentIndex + i > 12) {
+          this.holes[this.initalValue - k].push(newHoles[mainIndex][i]);
+          k = k + 1;
+        }
 
-    //     index2 += 1;
-
-    //   }
-    // }
+      }
+    }
+    mainIndex < 6 ? this.turn(false, true) : this.turn(true);
   }
+  // end:: #DefaultAction - taş sayısı 1'den fazla ise
 }
