fadf567c85f6f8cb63b02a3b21292afc62566d27
Nihat S <nhtsarimehmet@gmail.com>
2022-10-05 10:34:25 +0300

    app.component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import Swal from 'sweetalert2';
-import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  isShowAlert: boolean = true;
-
-  constructor(
-    private ngbModal: NgbModal,
-  ){}
-
-  ngOnInit(): void {
-  }
-
-  reset(){
-    Swal.fire({
-      title: 'Do you want to reset the game?',
-      text: '',
-      icon: 'warning',
-      showCancelButton: true,
-      customClass: { title: 'swal-style' },
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'No',
-      allowOutsideClick: false,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.reload();
-      }
-      else
-        Swal.close();
-    })
-  }
-
-  howToPlay(){
-    this.ngbModal.open(HowToPlayComponent, {size: 'xl'})
-  }
-
-}
+export class AppComponent {}
 
 
